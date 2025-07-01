6652e9e865399bf5a2cdf96748e559c84974ee4c
mrsaharg <winsahar5@gmail.com>
2025-07-02 00:53:27 +0500

    initial release

--- Student Management System/Controllers/StudentController.cs
+++ Student Management System/Controllers/StudentController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Student_Management_System.database;
+using Student_Management_System.Models.Entity;
 
 namespace Student_Management_System.Controllers
 {
@@ -13,9 +14,27 @@ namespace Student_Management_System.Controllers
             this.dbContext = dbContext;
         }
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var student = dbContext.Students.ToList();
+            var student = await dbContext.Students.ToListAsync();
+            return View(student);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        { 
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(Student student) {
+
+            if(student is not null && ModelState.IsValid)
+            {
+                await dbContext.Students.AddAsync(student);
+                await dbContext.SaveChangesAsync();
+                TempData["std_added"] = student.Name;
+                return RedirectToAction("Index");
+            }
+
             return View(student);
         }
 
