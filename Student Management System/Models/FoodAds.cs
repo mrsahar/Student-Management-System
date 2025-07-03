1e6d6d0dab5b2a00207484334fe90c4c6dfa53e2
mrsaharg <winsahar5@gmail.com>
2025-07-03 23:55:36 +0500

    Partial View for ads

---
+++ Student Management System/Models/FoodAds.cs
@@ -0,0 +1,11 @@
+﻿namespace Student_Management_System.Models
+{
+    public class FoodAds
+    {
+        public int Id { get; set; }
+        public required string Image { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public int Price { get; set; }
+    }
+}
